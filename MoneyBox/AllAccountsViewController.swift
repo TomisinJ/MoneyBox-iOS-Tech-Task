a92a1f3019205589677ff691c6d0220743724e19
Tomisin <tomisinjoseph3@gmail.com>
2023-11-28 16:26:14 +0000

    Button navigates to new page

---
+++ MoneyBox/AllAccountsViewController.swift
@@ -0,0 +1,18 @@
+//
+//  AllAccountsViewController.swift
+//  MoneyBox
+//
+//  Created by Tomisin on 28/11/2023.
+//
+
+import UIKit
+
+class AllAccountsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.view.backgroundColor = .green
+    }
+
+}
