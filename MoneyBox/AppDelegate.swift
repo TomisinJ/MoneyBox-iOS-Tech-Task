389d62e071aad98edfd363e822ce2e1fffb68755
Tomisin <tomisinjoseph3@gmail.com>
2023-11-29 12:20:46 +0000

    Updated app delegate

--- MoneyBox/AppDelegate.swift
+++ MoneyBox/AppDelegate.swift
@@ -11,6 +11,12 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow? 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        let viewController = LoginViewController() // Replace with your actual view controller
+            let navigationController = UINavigationController(rootViewController: viewController)
+            window?.rootViewController = navigationController
+            window?.makeKeyAndVisible()
+
         return true
     }
 }
